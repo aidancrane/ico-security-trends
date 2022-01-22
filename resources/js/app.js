91010d49adac3dd1a09e3370ec0a5ef102b34396
Aidan Crane <aidancrane78@gmail.com>
2022-01-22 20:11:48 +0000

    Swapped chartisan chart.js for echarts
    Not sure why but there you go.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 require('./bootstrap');
+
+//require('@chart.js');
+
+// Chartisan
+const { Chartisan } = require('@chartisan/echarts');
--- resources/views/totals.blade.php
+++ resources/views/totals.blade.php
@@ -4,7 +4,19 @@
 
 @section('content-right')
 <div class="container-fluid">
-    Hello World
+    <!-- Chart's container -->
+    <div id="chart"></div>
+    <!-- Charting library -->
+    {{-- <script src="https://unpkg.com/echarts/dist/echarts.min.js"></script> --}}
+    <!-- Chartisan -->
+    {{-- <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script> --}}
+    <!-- Your application script -->
+    <script>
+        const chart = new Chartisan({
+            el: '#chart',
+            url: "@chart('year_on_year_chart')",
+        });
+    </script>
 </div>
 @stop
 
