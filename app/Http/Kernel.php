ff9b14bab3e91e880f8deea588639d4e1912621a
Aidan Crane <aidancrane78@gmail.com>
2022-03-02 01:14:47 +0000

    Added caching
    Proxys and caching is cool

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,9 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+
+            // NOTE if you want to do some session stuff you are behind a cache and proxy!
+
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,12 +6,17 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
+
+    // disable cookies entirely.
+    protected $addHttpCookie = false;
+
     /**
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array<int, string>
      */
     protected $except = [
-        //
+        // Remove all csrf middleware because we want our proxy to cache everything.
+        '*',
     ];
 }
