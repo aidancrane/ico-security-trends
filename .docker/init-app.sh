1ddd01f861d600f9fb48619eb15b584ba47e0f45
Aidan Crane <aidancrane78@gmail.com>
2022-03-13 23:03:32 +0000

    Added NPM to development build
    So you can now use npm from app command line.
    
    Also updated npm packages so you can see why I needed to do this...

--- .docker/init-app.sh
+++ .docker/init-app.sh
@@ -2,9 +2,13 @@
 # Install composer dependencies
 #composer install
 echo "Waiting on Database to startup before re-seeding database."
+echo "If you see Error 502 Bad Gateway please wait for the database to seed first it can take a few seconds."
 # # Wait for DB to be up
 /usr/local/bin/wait-for-it.sh db:3306 -t 0 --strict -- php artisan key:generate && php artisan migrate:refresh --seed
 #
 echo "Database has been completely re-seeded from source."
+echo "Running all Mix Tasks."
+
+npm run dev
 
 php-fpm
