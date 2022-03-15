b789f49e19b4bc1b7069f71b10a4ec6e4edb4e8c
Aidan Crane <aidancrane78@gmail.com>
2022-03-15 00:23:48 +0000

    Added stats to main page.
    Also back-tracked chart.js as is not compatible with chartisan.

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ICOQuarter;
+use App\Models\ICOIncidentCount;
+use App\Models\ICOCategory;
 
 class LandingController extends Controller
 {
@@ -15,7 +17,53 @@ class LandingController extends Controller
        $highest_q = ICOQuarter::orderBy('data_range_end', 'desc')->orderBy('quarter_1234', 'desc')->first();
        $lowest_q = ICOQuarter::orderBy('data_range_end', 'asc')->orderBy('quarter_1234', 'asc')->first();
 
-       return view('landing')->with('highest_q', $highest_q)->with('lowest_q', $lowest_q);
+       $most_reported_crime_category = ICOIncidentCount::all()->sum('incident_count');
+
+       //Get a list of categories,
+       $data_categories = ICOCategory::all();
+       $data_incidents = ICOIncidentCount::all();
+
+       $category_incident_counts_by_id = [];
+
+       for ($y = 0; $y <= ($data_categories->count() - 1); $y++) {
+         $this_category = $data_categories->values()->get($y);
+         $this_category_id = $this_category->id;
+
+         for ($z = 0; $z <= ($data_incidents->count() - 1); $z++) {
+             $this_incident = $data_incidents->values()->get($z);
+             //dd($this_incident);
+             $this_incident_category_id = $this_incident['ico_category_id'];
+
+             if(isset($category_incident_counts_by_id[$this_incident_category_id])){
+                $category_incident_counts_by_id[$this_incident_category_id] = $category_incident_counts_by_id[$this_incident_category_id] + $this_incident['incident_count'];
+             }
+             else {
+               $category_incident_counts_by_id[$this_incident_category_id] = $this_incident['incident_count'];
+             }
+
+         }
+
+       }
+
+
+       $this_years_quarters = ICOQuarter::orderBy('data_range_end', 'asc')->orderBy('quarter_1234', 'asc')->where('data_range_start', '>=' , date("Y") - 1);
+       $this_years_incidents = $this_years_quarters->with('ICOIncidents')->get()->pluck('ICOIncidents')->flatten();
+       $sum_of_this_years_incidents = $this_years_incidents->sum('incident_count');
+
+       $last_years_quarters = ICOQuarter::orderBy('data_range_end', 'asc')->orderBy('quarter_1234', 'asc')->where('data_range_start', '>=' , date("Y") - 2)->where('data_range_start', '<' , date("Y") - 1);
+       $last_years_incidents = $last_years_quarters->with('ICOIncidents')->get()->pluck('ICOIncidents')->flatten();
+       $sum_of_last_years_incidents = $last_years_incidents->sum('incident_count');
+
+
+       $most_reported_crime_category_id = array_search(max($category_incident_counts_by_id), $category_incident_counts_by_id);
+       $most_reported_crime_category = ICOCategory::where('id', $most_reported_crime_category_id)->first();
+       //dd($most_reported_crime_category);
+       return view('landing')->with('highest_q', $highest_q)
+       ->with('lowest_q', $lowest_q)
+       ->with('most_reported_crime_category', $most_reported_crime_category)
+       ->with('sum_of_this_years_incidents', $sum_of_this_years_incidents)
+       ->with('last_year', date("Y") - 1)
+       ->with('sum_of_last_years_incidents', $sum_of_last_years_incidents);
      }
 
 }
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -14,11 +14,11 @@
                 <p>This dashboard aims to provide the most up to date Information Commissioners Office security incident trends as reported by government
                     organisations and private companies.</p>
 
-                <p>Data security incidents, which are breaches of the seventh data protection principle or personal data breaches
-                    reported under the Privacy and Electronic Communications Regulations are a major concern for those affected.</p>
+                <p>Data security incidents are specifically breaches of the seventh data protection principle or personal data breaches
+                    reported under the Privacy and Electronic Communications Regulations and a major concern for those affected.</p>
 
                 <p>The UK Government body, called the Information Commissioners Office publish this information in non-standardised spreadsheets.
-                    I have taken the time to format this data so that it has normalised and proper data structure.</p>
+                    This website breaks down the information collated to provide insight and overview of the data contained inside the spreadsheets.</p>
 
                 <div class="alert alert-secondary mt-1 mb-1 py-1" role="alert">
                     <div class="small my-0 py-0">
@@ -32,14 +32,13 @@
                             <li>
                                 <span class="mdi mdi-license"></span> Alternatively you can read the terms of the licence <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">here</a>
                             </li>
+                            <li>
+                                <span class="mdi mdi-rotate-left"></span> Data from <b>Q{{ $lowest_q->quarter_1234 }} {{ $lowest_q->data_range_start }}-{{ $lowest_q->data_range_end }}</b>
+                                to <b>Q{{ $highest_q->quarter_1234 }} {{ $highest_q->data_range_start }}-{{ $highest_q->data_range_end }}</b>
+                            </li>
                         </ul>
                     </div>
                 </div>
-
-                <p>Data from <b>Q{{ $lowest_q->quarter_1234 }} {{ $lowest_q->data_range_start }}-{{ $lowest_q->data_range_end }}</b>
-                    to <b>Q{{ $highest_q->quarter_1234 }} {{ $highest_q->data_range_start }}-{{ $highest_q->data_range_end }}</b>.
-                    Further data for the latest quarter may be available on the official website.</p>
-
             </div>
             <div class="col-md-6">
                 <div class="lead fw-normal np-0 b-0 g-0">
@@ -64,6 +63,28 @@
                         </div>
                     </a>
                 </ol>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="lead fw-normal np-0 b-0 g-0 pt-2">
+                        Quick Statistics
+                    </div>
+
+                    <ul class="list my-0 py-0">
+                        <li>
+                            The most reported cyber security incident to the UK ICO is categorised as <span title="{{ $most_reported_crime_category->definition }}"><b>{{ $most_reported_crime_category->category }}</b></span>
+                        </li>
+                        <li>
+                            So far there have been <b>{{ $sum_of_this_years_incidents }}</b> security incidents reported to the UK ICO for this years quarters*
+                        </li>
+                        <li>
+                            Last year there were <b>{{ $sum_of_last_years_incidents }}</b> security incidents reported to the UK ICO*
+                        </li>
+                    </ul>
+
+                    <small>*<i> Data annualised, ~{{ $last_year-1 }}-{{ $last_year }}</i></small>
+                </div>
             </div>
         </div>
     </div>
